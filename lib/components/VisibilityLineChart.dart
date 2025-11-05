1664740c720558e73f8a57f571ac73464b9f29e7
pghuy1510 <phamhuyyk1510@gmail.com>
2025-11-05 23:03:07 +0700

    Create_ VisibilityLineChart_file

---
+++ lib/components/VisibilityLineChart.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
+
+class Visibilitylinechart extends StatefulWidget {
+  const Visibilitylinechart({super.key});
+
+  @override
+  State<Visibilitylinechart> createState() => _VisibilitylinechartState();
+}
+
+class _VisibilitylinechartState extends State<Visibilitylinechart> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
