8b0e73180887929fcb328a67f4f0a87682755494
pghuy1510 <phamhuyyk1510@gmail.com>
2025-11-05 23:10:01 +0700

    Create_PressureLineChart_file

---
+++ lib/components/PressureLineChart.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
+
+class Pressurelinechart extends StatelessWidget {
+  const Pressurelinechart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
